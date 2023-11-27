b87e3d434c64070a6000dfd31f8951350c49b648
DevJunior <92763121+oclubedosdez@users.noreply.github.com>
2023-11-27 15:34:51 -0300

    Add files via upload

---
+++ mangas/It’s Time to Change the Genre/script.js
@@ -0,0 +1,27 @@
+var capitulos = document.querySelector('.container')
+
+var num = 46 //informe quantas pastas tera o manga/manhaw
+
+let pastas = [];
+
+for (let i = 1; i <= num; i++) {
+    pastas.push({numero : `${i < 10 ? "0" : ""}${i}`, nome : `Capitulo ${i < 10 ? "0" : ""}${i}`})
+}
+
+console.log(pastas)
+
+pastas.forEach((pasta) => {
+    let pasta_element = `
+    <div class="box">
+            <div class="imagen">
+                <a href="./capitulo-${pasta.numero}/index.html">
+                    <img src="./pasta.png" alt="">
+                </a>
+            </div>
+            <div class="texto">
+                <span>${pasta.nome}</span>
+            </div>
+        </div>
+    `;
+    capitulos.innerHTML += pasta_element;
+})
